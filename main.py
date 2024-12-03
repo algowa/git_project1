bfc2c2c3641db7293943ed087ce9d9e1a125f24d
AlgoZ <algowa.z@gmail.com>
2024-12-03 18:33:56 +0200

    Multiply

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def my_superfunction():
+def mp():
     print(3 * 3)
 
 
@@ -7,7 +7,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-    my_superfunction()
+    mp()
 
 
 if __name__ == '__main__':
